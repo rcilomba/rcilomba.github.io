cbd3b5b22a80d66a6ff0f0e8b4e2d5eccb499209
rcilomba <32643822+rcilomba@users.noreply.github.com>
2022-06-23 00:48:09 +0200

    Delete active.js
    re

--- active.js
+++
@@ -1,9 +0,0 @@
-let menu = document.querySelector(".menu");
-let active = menu.querySelectorAll(".active");
-for (var i = 0; i < active.length; i++) {
-    active[i].addEventListener("click", function() {
-        var current = document.getElementsByClassName("active");
-        current[0].className = current[0].className.replace(" active", "");
-        this.className += " active";
-    });
-}
